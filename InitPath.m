ca7e44086aec056cb6c763b97a4b8417cb409693
cjouny <cjouny@jhmi.edu>
2015-09-10 11:50:33 -0400

    Add back InitPath

---
+++ InitPath.m
@@ -0,0 +1,9 @@
+% Initialize your path with this folder version of ooMEFBrowser
+Rpath=pwd;
+oomef='OOmef';
+tools='tools';
+
+addpath(Rpath);
+addpath(fullfile(Rpath, oomef));
+addpath(fullfile(Rpath, tools));
+
